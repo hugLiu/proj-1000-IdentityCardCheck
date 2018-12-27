17ca28f34bce8a9f50a5499958efbd14c33bc7e3
hugLiu <lliu606@hotmail.com>
2018-12-27 18:14:22 +0800

    code

---
+++ IdentityCardCheck/IdentityCardCheck/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace IdentityCardCheck
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tb_id = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.校验 = new System.Windows.Forms.Button();
+            this.lab_error = new System.Windows.Forms.Label();
+            this.lab_num = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tb_id
+            // 
+            this.tb_id.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.tb_id.Location = new System.Drawing.Point(12, 34);
+            this.tb_id.Name = "tb_id";
+            this.tb_id.Size = new System.Drawing.Size(239, 26);
+            this.tb_id.TabIndex = 0;
+            this.tb_id.TextChanged += new System.EventHandler(this.tb_id_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "身份证号";
+            // 
+            // 校验
+            // 
+            this.校验.Location = new System.Drawing.Point(162, 68);
+            this.校验.Name = "校验";
+            this.校验.Size = new System.Drawing.Size(75, 23);
+            this.校验.TabIndex = 2;
+            this.校验.Text = "开始校核";
+            this.校验.UseVisualStyleBackColor = true;
+            this.校验.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lab_error
+            // 
+            this.lab_error.AutoEllipsis = true;
+            this.lab_error.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lab_error.Location = new System.Drawing.Point(9, 119);
+            this.lab_error.Name = "lab_error";
+            this.lab_error.Size = new System.Drawing.Size(270, 134);
+            this.lab_error.TabIndex = 3;
+            // 
+            // lab_num
+            // 
+            this.lab_num.AutoSize = true;
+            this.lab_num.Location = new System.Drawing.Point(162, 16);
+            this.lab_num.Name = "lab_num";
+            this.lab_num.Size = new System.Drawing.Size(83, 12);
+            this.lab_num.TabIndex = 4;
+            this.lab_num.Text = "已录入字数：0";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.lab_num);
+            this.Controls.Add(this.lab_error);
+            this.Controls.Add(this.校验);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tb_id);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmMain";
+            this.Text = "身份证号有效性校核";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tb_id;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button 校验;
+        private System.Windows.Forms.Label lab_error;
+        private System.Windows.Forms.Label lab_num;
+    }
+}
+
---
+++ IdentityCardCheck/IdentityCardCheck/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace IdentityCardCheck
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            
+            lab_error.Text = CheckIDCard(tb_id.Text );
+        }
+
+
+         public string CheckIDCard(string idNumber)  
+        {
+      
+             string errorMessage="";
+
+            if (idNumber.Length == 18)  
+            {
+                errorMessage = CheckIDCard18(idNumber);  //18位校核 ，正确返回true，错误返回false
+            }  
+            else if (idNumber.Length == 15)  
+            {
+                errorMessage = CheckIDCard15(idNumber);//15位校核，正确返回true，错误返回false
+            }  
+            else  //身份证号长度不是15或18位
+            {
+ 
+                    errorMessage = "身份证号应为18位或15位长度。";
+                 
+            }
+             return errorMessage;
+            
+           
+
+
+        }
+       
+
+        /// <summary>
+        /// 18位身份证号码验证
+        /// </summary>
+        /// <param name="idNumber"></param>
+        /// <returns></returns>
+        public string  CheckIDCard18(string idNumber )
+        {
+            long n = 0;
+            if (long.TryParse(idNumber.Remove(17), out n) == false || n < Math.Pow(10, 16) || long.TryParse(idNumber.Replace('x', '0').Replace('X', '0'), out n) == false)
+            {
+                  return "数字验证失败。" ;//数字验证  
+            }
+            string address = "11x22x35x44x53x12x23x36x45x54x13x31x37x46x61x14x32x41x50x62x15x33x42x51x63x21x34x43x52x64x65x71x81x82x91";
+            if (address.IndexOf(idNumber.Remove(2)) == -1)
+            {
+                return "省份位"+idNumber.Remove(2)+"验证失败，前两位应为11|22|35|44|53|12|23|36|45|54|13|31|37|46|61|14|32|41|50|62|15|33|42|51|63|21|34|43|52|64|65|71|81|82|91。";//省份验证  
+            }
+            string birth = idNumber.Substring(6, 8).Insert(6, "-").Insert(4, "-");
+            DateTime time = new DateTime();
+            if (DateTime.TryParse(birth, out time) == false)
+            {
+               return "生日位"+birth+"验证失败。" ;;//生日验证  
+            }
+            string[] arrVarifyCode = ("1,0,x,9,8,7,6,5,4,3,2").Split(',');
+            string[] Wi = ("7,9,10,5,8,4,2,1,6,3,7,9,10,5,8,4,2").Split(',');
+            char[] Ai = idNumber.Remove(17).ToCharArray();
+            int sum = 0;
+            for (int i = 0; i < 17; i++)
+            {
+                sum += int.Parse(Wi[i]) * int.Parse(Ai[i].ToString());
+            }
+            int y = -1;
+            Math.DivRem(sum, 11, out y);
+            Console.WriteLine("Y的理论值: " + y);
+            if (arrVarifyCode[y] != idNumber.Substring(17, 1).ToLower())
+            {
+                return "校验码验证失败，应为"+arrVarifyCode[y]+"。";//校验码验证  
+            }
+            return "通过校验！";//符合GB11643-1999标准  
+        }
+
+
+
+
+
+  
+        /// <summary>  
+        /// 15位身份证号码验证  
+        /// </summary>  
+        private string  CheckIDCard15(string idNumber)  
+        {  
+            long n = 0;  
+            if (long.TryParse(idNumber, out n) == false || n < Math.Pow(10, 14))  
+            {  
+                 return "数字验证失败。" ;//数字验证  
+            }  
+            string address = "11x22x35x44x53x12x23x36x45x54x13x31x37x46x61x14x32x41x50x62x15x33x42x51x63x21x34x43x52x64x65x71x81x82x91";  
+            if (address.IndexOf(idNumber.Remove(2)) == -1)  
+            {  
+                 return "省份位"+idNumber.Remove(2)+"验证失败，前两位应为11|22|35|44|53|12|23|36|45|54|13|31|37|46|61|14|32|41|50|62|15|33|42|51|63|21|34|43|52|64|65|71|81|82|91。";//省份验证  
+            }  
+            string birth = idNumber.Substring(6, 6).Insert(4, "-").Insert(2, "-");  
+            DateTime time = new DateTime();  
+            if (DateTime.TryParse(birth, out time) == false)  
+            {  
+                return "生日位"+birth+"验证失败。" ;//生日验证  
+            }
+            return "通过校验！";  
+        }   
+
+       
+
+        private void tb_id_TextChanged(object sender, EventArgs e)
+        {
+            
+                lab_num.Text =" 已录入字数："+tb_id.Text.Length.ToString();
+        }
+
+
+    }
+}
